679a9f1efecde535428c8dbbe578ef9458a47779
kky <smallkangzi@qq.com>
2018-02-28 15:52:47 +0800

    修改方法位置

--- setup.py
+++ setup.py
@@ -3,24 +3,23 @@ __author__ = 'kky'
 
 from setuptools import setup, find_packages
 
-
-
 setup(
-    name='pytools',
-    version='0.0.1',
+    name='Pytools',
+    version='0.0.9',
     description='a python3 tools lib',
     url='',
     author='kky',
     author_email='kkyflying@gmail.com',
     license='MIT',
     keywords='tools',
-    packages=find_packages('src'),
-    python_requires='>=3',
-    classifiers=[
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 3.5',
-    ],
+    packages=find_packages(),
+
 )
+
+# classifiers=[
+#     #   3 - Alpha
+#     #   4 - Beta
+#     #   5 - Production/Stable
+#     'Development Status :: 3 - Alpha',
+#     'Programming Language :: Python :: 3.5',
+# ]
