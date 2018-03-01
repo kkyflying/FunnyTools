d5a2e40d2673b8d052d9667c5f5862058dbe697b
kky <kkyflying@gmail.com>
2018-03-01 11:39:04 +0800

    修改项目名字

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ from setuptools import setup, find_packages
 # python3 setup.py sdist     # 生成pip支持的格式，下文以此为例
 # sudo pip3 install -e PyTools/
 setup(
-    name='Pytools',
+    name='funnytools',
     version='0.0.1',
-    description='a python3 tools lib',
-    url='https://github.com/kkyflying/PyTools',
+    description='this is a funny python3 tools',
+    url='https://github.com/kkyflying/FunnyTools',
     author='kky',
     author_email='kkyflying@gmail.com',
     license='MIT',
-    keywords='tools',
+    keywords='python3 tools',
     packages=find_packages(),
     platforms = 'linux mac',
     classifiers=[
